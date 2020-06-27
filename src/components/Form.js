3ceb0dc6997390a5697fc70a49ddb8d9a0964c77
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-06-27 17:49:08 +0200

    skeleton done

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Button = styled.input`
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 20px;
+  padding: 10px;
+  background-color: #66a2fe;
+  border: none;
+  width: 100%;
+  border-radius: 10px;
+  color: #fff;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #326ac0;
+    cursor: pointer;
+  }
+`;
+
+const From = () => {
+  return (
+    <form>
+      <Button type="submit" value="Calc" />
+    </form>
+  );
+};
+
+export default From;
